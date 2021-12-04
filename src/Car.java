599c0a9b5585732a9c6710ad71000ed4431cbc23
Artem Meshcherov <spacenail@gmail.com>
2021-12-04 22:08:27 +0300

    add СyclicBarrier,CountDownLatch,Semaphore

--- src/Car.java
+++ src/Car.java
@@ -1,34 +1,67 @@
+import java.util.concurrent.BrokenBarrierException;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
+
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
-            System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
-            System.out.println(this.name + " готов");
+            preparing();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        start();
+    }
+
+    private void preparing() throws InterruptedException, BrokenBarrierException {
+        System.out.println(this.name + " готовится");
+        Thread.sleep(500 + (int) (Math.random() * 800));
+        System.out.println(this.name + " готов");
+        MainClass.END_OF_PREPARED.countDown();
+        MainClass.CYCLIC_BARRIER.await();
+    }
+
+    private void start() {
         for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+            Stage stage = race.getStages().get(i);
+            if (stage instanceof Tunnel) {
+                try {
+                    MainClass.TUNNEL.acquire();
+                    stage.go(this);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    MainClass.TUNNEL.release();
+                }
+            } else {
+                stage.go(this);
+            }
         }
+        System.out.println(name + " прошел все этапы!");
+        MainClass.END_OF_RACE.countDown();
     }
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -4,24 +4,41 @@
 каждого из них уходит разное время.//CyclicBarrier
 В туннель не может заехать одновременно больше половины участников (условность).
 Попробуйте всё это синхронизировать.//Semaphore
-Только после того как все завершат гонку, нужно выдать объявление об окончании.//join
+Только после того как все завершат гонку, нужно выдать объявление об окончании.
 Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.
  */
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    static final int CARS_COUNT = 4;
+    static final CyclicBarrier CYCLIC_BARRIER = new CyclicBarrier(CARS_COUNT);
+    static final CountDownLatch END_OF_PREPARED = new CountDownLatch(CARS_COUNT);
+    static final CountDownLatch END_OF_RACE = new CountDownLatch(CARS_COUNT);
+    static final Semaphore TUNNEL = new Semaphore(CARS_COUNT/2);
+
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));//создание трассы
+        Car[] cars = new Car[CARS_COUNT];//создание участников
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
-        for (int i = 0; i < cars.length; i++) {
+
+        for (int i = 0; i < cars.length; i++) { //запуск отдельного потока для каждой машины
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+
+        try {
+            END_OF_PREPARED.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            END_OF_RACE.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
